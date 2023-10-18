c63b2f5b598ac454f48146748d8b2417cdcabe41
Fernando <fescalona@immediait.com>
2023-10-18 13:42:53 +0200

    add eslint configuration

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,8 @@ import { PORT } from './config.js'
 
 const app = express()
 
-app.get('/', (req, res) => {
-    console.log(req)
-    return res.status(234).send('Welcome to MERN Stack Tutorial')
-})
+app.get('/', (req, res) => res.status(234).send('Welcome to MERN Stack Tutorial'))
 
 app.listen(PORT, () => {
-    console.log(`App is running in ${PORT}`);
+    console.log(`App is running in ${PORT}`)
 })
